561ec6f82de662aaa69727a48380995740ad35f7
Julie <Juliemk65@gmail.com>
2024-12-10 19:37:39 -0500

    backend routes work to add and find clients 12/10/24

--- backend/clientRoutes.js
+++ backend/clientRoutes.js
@@ -16,29 +16,32 @@ clientRoutes.route("/clients").get(async (req, res) => {
   }
 });
 //#2-Retrieve ONE
-//http://localhost:3000/clients/:name
+//http://localhost:3000/clients/Julie
 //new ObjectId(req.params.name)
 //Object.keys(data).length > 0
-clientRoutes.route("/clients/:name").get(async (req, res) => {
+clientRoutes.route("/clients/:clientName").get(async (req, res) => {
   let db = database.getDb();
-  let data = await db.collection("clients").findOne({ name: req.params.name });
+  let data = await db
+    .collection("clients")
+    .findOne({ name: req.params.clientName });
+
   if (data) {
-    res.json(Object.keys(data).length > 0);
+    res.json(data);
   } else {
     throw new Error("Data was not found :(");
   }
 });
-//#3-Create One
-//http://localhost:3000/clients
-clientRoutes.route("/clients").post(async (req, res) => {
+//#3-Create/Add One
+//http://localhost:3000/clientsAdd
+clientRoutes.route("/clientsAdd").post(async (req, res) => {
   let db = database.getDb();
   let mongoObject = {
-    name: request.body.name,
-    dog_1: request.body.dog_1,
-    dog_2: request.body.dog_2,
-    dog_3: request.body.dog_3,
-    address: request.body.address,
-    repeat_client: request.body.repeat_client,
+    name: req.body.name,
+    dog_1: req.body.dog_1,
+    dog_2: req.body.dog_2,
+    dog_3: req.body.dog_3,
+    address: req.body.address,
+    repeat_client: req.body.repeat_client,
   };
   let data = await db.collection("clients").insertOne(mongoObject);
   res.json(data);
--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -12,9 +12,10 @@ function Admin() {
   const [clientName, setClientName] = useState("");
   const [results, setResults] = useState("");
 
-  useEffect(() => {
+  //use to immediately see all clients
+  /*useEffect(() => {
     async function grabData() {
-      const response = await axios.get("http://localhost:3000/clients/Julie");
+      const response = await axios.get("http://localhost:3000/clients");
       console.log(response);
       if (response.status === 200) {
         setResults(response.data);
@@ -22,64 +23,108 @@ function Admin() {
     }
 
     grabData();
-  }, []);
+  }, []);*/
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     axios
-  //       .post("http://localhost:3000/adminPage", {
-  //         name,
-  //         dog_1,
-  //         dog_2,
-  //         dog_3,
-  //         address,
-  //       })
-  //       .then((result) => {
-  //         console.log(result.data);
-  //         alert("New Client created, thank you! 🐶🫶🏼🐱");
-  //       });
-  //   } catch (error) {
-  //     //   .catch((err) => console.log(err));
-  //     console.error("Error fetching data", error);
-  //   }
-  // };
+  //use to immediately see info of client that's name is in 'Existing client Name'
+  /*useEffect(() => {
+    async function grabData() {
+      const response = await axios.get(
+        `http://localhost:3000/clients/${clientName}`,
+        {
+          clientName,
+        }
+      );
+      console.log(response);
+      if (response.status === 200) {
+        setResults(response.data);
+      }
+    }
 
-  // const handleSubmitFind = (e) => {
-  //   e.preventDefault();
-  //   const results = document.getElementById("results");
-  //   const dogImage = document.getElementById("dog-image");
-  //   results.textContent = "";
+    grabData();
+  }, []);*/
 
-  //   try {
-  //     axios
-  //       .post("http://localhost:3000/clients/:name", {
-  //         name,
-  //         clientName,
-  //         address,
-  //       })
-  //       .then((result) => {
-  //         console.log(`client-side response`, result.data);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      async function pushData() {
+        const response = await axios
+          .post("http://localhost:3000/clientsAdd", {
+            name,
+            dog_1,
+            dog_2,
+            dog_3,
+            address,
+          })
+          .then((response) => {
+            console.log(response.config.data);
+            alert("New Client created, thank you! 🐶🫶🏼🐱");
+          });
+      }
+      pushData();
+    } catch (error) {
+      //   .catch((err) => console.log(err));
+      console.error("Error fetching data", error);
+    }
+  };
 
-  //         //   setResults(result.data);
-  //         const nameElement = document.createElement("p");
-  //         nameElement.className = "client-results-p";
-  //         nameElement.textContent = `${result.data}`;
+  const handleSubmitFind = (e) => {
+    e.preventDefault();
+    const results = document.getElementById("results");
+    const dogImage = document.getElementById("dog-image");
+    results.textContent = "";
 
-  //         // Append the elements to the "dataDisplay" div
-  //         results.appendChild(nameElement);
+    try {
+      async function grabData() {
+        const response = await axios
+          .get(`http://localhost:3000/clients/${clientName}`, {
+            clientName,
+          })
+          .then((response) => {
+            console.log(response.data);
 
-  //         if (clientName) {
-  //           dogImage.innerHTML = `<img src="Images/${clientName}.jpeg"
-  //           alt="${clientName}'s Animal photo"
-  //           id="pup-img"
-  //           width=250px>`;
-  //         }
-  //       });
-  //   } catch (error) {
-  //     console.error("Error fetching data", error);
-  //   }
-  // };
+            //   setResults(response.data);
+            const nameElement = document.createElement("p");
+            nameElement.className = "client-results-p";
+            if (
+              response.data.dog_2 === undefined ||
+              response.data.dog_2 === ""
+            ) {
+              nameElement.textContent = `Client Name: ${response.data.name} \n
+            Dog Name: ${response.data.dog_1} \n
+            Address: ${response.data.address}\n
+            Repeat Client: ${response.data.repeat_client}`;
+            } else if (
+              response.data.dog_3 === undefined ||
+              response.data.dog_3 === ""
+            ) {
+              nameElement.textContent = `Client Name: ${response.data.name} \n
+            Dog Names: ${response.data.dog_1}, ${response.data.dog_2}  \n
+            Address: ${response.data.address}\n
+            Repeat Client: ${response.data.repeat_client}`;
+            } else {
+              nameElement.textContent = `Client Name: ${response.data.name} \n
+            Dog Names: ${response.data.dog_1}, ${response.data.dog_2}, ${response.data.dog_3}   \n
+            Address: ${response.data.address}\n
+            Repeat Client: ${response.data.repeat_client}`;
+            }
+
+            // Append the elements to the "dataDisplay" div
+            results.appendChild(nameElement);
+
+            if (clientName) {
+              dogImage.innerHTML = `<img src="Images/${clientName}.jpeg"
+            alt="${clientName}'s Animal photo"
+            id="pup-img"
+            width=250px>`;
+            }
+          });
+      }
+
+      grabData();
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
 
   return (
     <>
@@ -88,7 +133,7 @@ function Admin() {
         <Link to="/loginPage" className="go-back-button-admin">
           Go Back
         </Link>
-        <form className="create-client-form">
+        <form className="create-client-form" onSubmit={handleSubmit}>
           <label htmlFor="name">Client Name:</label>
           <input name="name" onChange={(e) => setName(e.target.value)}></input>
           <label htmlFor="dog_1">Dog Name (1):</label>
@@ -112,7 +157,7 @@ function Admin() {
             onChange={(e) => setAddress(e.target.value)}
           ></input>
 
-          {/* <button
+          <button
             type="submit"
             onClick={handleSubmit}
             className="create-button"
@@ -125,20 +170,21 @@ function Admin() {
           <label>Existing client name:</label>
           <input
             name="clientName"
+            value={clientName}
             onChange={(e) => setClientName(e.target.value)}
-          /> */}
+          />
           <button type="submit" className="find-button">
             Find Button
           </button>
         </form>
-        {/* <div
+        <div
           id="results"
           onChange={(e) => setResults(e.target.value)}
           value={setResults}
           className="client-results"
         >
           {results.data}
-        </div> */}
+        </div>
         {JSON.stringify(results.data)}
         <div id="dog-image" className="animal-img"></div>
       </div>
